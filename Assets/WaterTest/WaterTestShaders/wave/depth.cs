8a138d4b88fd599298af905ccbac7d35337525aa
Macx <Macx@drkdeMacBook-Pro.local>
2016-07-21 12:04:32 +0800

    add some water shader

---
+++ Assets/WaterTest/WaterTestShaders/wave/depth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class depth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.camera.depthTextureMode = DepthTextureMode.Depth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
